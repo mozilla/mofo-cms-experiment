9922b315aeee76a580ec3a71c79f8d0587f9880d
Mavis Ou <mmmavisss@gmail.com>
2016-02-03 20:54:59 -0800

    add 'Mofo CMS Experiment' ribbon

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ export default class Index extends React.Component {
   render() {
     return (
       <div>
+        <div id="cms-exp-label"><div>Mofo CMS Experiment</div></div>
         <Navbar />
         <div id="main-container" className="container">
           {this.props.children}
