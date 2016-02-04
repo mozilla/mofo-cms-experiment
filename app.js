e956013898716585008afd4faae5e9f53b160bdc
Mavis Ou <mmmavisss@gmail.com>
2016-02-03 17:31:36 -0800

    package.json changes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import path from 'path';
-import bodyParser from 'body-parser';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { match, RoutingContext } from 'react-router';
